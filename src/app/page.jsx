030f88efb5adbc7c04277e561a66560f8e961931
Muhammad Zohaib Ul Hassan <zohaibfas@gmail.com>
2023-07-28 17:19:55 +0500

    hero section done
    hero section complete all done here

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,23 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import hero from "public/hero.png";
 
 export default function Home() {
   return (
-    <div>
-      <h2>This is Zohaib Ul Hassan</h2>
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.heading}>
+          better designs for your digital products
+        </h1>
+        <p className={styles.des}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui mollitia
+          reiciendis voluptate maxime a repudiandae .
+        </p>
+        <button className={styles.button}>see our work</button>
+      </div>
+      <div className={styles.item}>
+        <Image src={hero} className={styles.image} />
+      </div>
     </div>
   );
 }
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,11 +6,35 @@ const Footer = () => {
   return (
     <footer className={styles.container}>
       <div>&copy;copywrite reserved 2023</div>
-      <div className={styles}>
-        <Image src="/1.png" width={15} height={15} alt="facebook" />
-        <Image src="/2.png" width={15} height={15} alt="instagram" />
-        <Image src="/3.png" width={15} height={15} alt="twitter" />
-        <Image src="/4.png" width={15} height={15} alt="youtube" />
+      <div className={styles.social}>
+        <Image
+          src="/1.png"
+          width={15}
+          height={15}
+          className={styles.icons}
+          alt="facebook"
+        />
+        <Image
+          src="/2.png"
+          width={15}
+          height={15}
+          className={styles.icons}
+          alt="instagram"
+        />
+        <Image
+          src="/3.png"
+          width={15}
+          height={15}
+          className={styles.icons}
+          alt="twitter"
+        />
+        <Image
+          src="/4.png"
+          width={15}
+          height={15}
+          className={styles.icons}
+          alt="youtube"
+        />
       </div>
     </footer>
   );
